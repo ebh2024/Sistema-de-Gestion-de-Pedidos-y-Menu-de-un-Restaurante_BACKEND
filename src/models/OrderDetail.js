6e055ea88e350f9ef80e2a90e6ae9a82263c1b2a
Facu Toledo <fa.toledo@itecriocuarto.org.ar>
2025-10-14 19:23:48 -0300

    Definir relaciones entre modelos y agregar claves foráneas (userId, tableId, orderId, dishId)

--- src/models/OrderDetail.js
+++ src/models/OrderDetail.js
@@ -2,6 +2,22 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const OrderDetail = sequelize.define('OrderDetail', {
+  orderId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'orders',
+      key: 'id'
+    }
+  },
+  dishId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'dishes',
+      key: 'id'
+    }
+  },
   quantity: {
     type: DataTypes.INTEGER,
     allowNull: false
